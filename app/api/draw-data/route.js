a25820c7bc87d5e5a48b315a21572bfda2e66256
Itamar Kenan <123303818+ItamarKenan@users.noreply.github.com>
2026-04-13 18:13:54 +0300

    Added first route

--- app/api/draw-data/route.js
+++ app/api/draw-data/route.js
@@ -9,18 +9,16 @@ export async function GET() {
 
     const xmlText = await response.text();
     const parser = new XMLParser({
-      ignoreAttributes: false, // קריטי! אומר לו לא להתעלם מהנתונים כמו c1="J"
-      attributeNamePrefix: ""  // מנקה קידומות מיותרות כדי שהשמות יהיו נקיים (c1 ולא @_c1)
+      ignoreAttributes: false,
+      attributeNamePrefix: ""
     });
 
-    // מתרגמים בפועל!
+    
     const jsonObj = parser.parse(xmlText);
 
-    // 3. שולחים את התשובה כ-JSON נקי חזרה למי שביקש (לפרונטאנד שלנו)
     return NextResponse.json(jsonObj);
     
   } catch (error) {
-    // אם משהו משתבש (למשל אין אינטרנט), נחזיר שגיאה מסודרת
     return NextResponse.json({ error: 'שגיאה במשיכת הנתונים' }, { status: 500 });
   }
 }
